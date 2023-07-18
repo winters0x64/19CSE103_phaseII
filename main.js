51bd8703014f92fbf1baf4489917422107c642d9
winters0x64 <arun0x168codes@gmail.com>
2023-07-18 10:39:33 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const apikey = "e6e95ac1"
+const base_uri = "https://www.omdbapi.com/?apikey="
+
+console.log("hi")
+
+let inp = document.getElementById("movie_name");
+
+let createForm = (data)=>{
+    var x = document.getElementById("data")
+    if (x) x.remove()
+    let content = document.createElement("div");
+    content.id  = "data"
+    content.innerHTML += `<img src=${data.Poster} onerror=alert("Failed to load Image")>`
+    content.innerHTML += `<h4>Movie Name : ${data.Title}</h4><br>`+`<h4>Release Year : ${data.Year}</h4><br>`+ `<h4>Genre : ${data.Genre}</h4><br>`
+    content.innerHTML += `<h4>Plot : ${data.Plot}</h4><br>`+`<h4>Actors : ${data.Actors}</h4><br>`
+    document.body.appendChild(content)
+}
+
+document.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    fetch(base_uri+apikey+'&t='+inp.value)
+    .then(res=>res.json())
+    .then(data=>createForm(data))
+    .catch(err=>console.log(err))
+})
+
+
