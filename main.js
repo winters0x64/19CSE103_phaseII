7a0a3455267c0a6d5891d29450886c133ff2db49
winters0x64 <arun0x168codes@gmail.com>
2023-07-24 01:39:45 +0530

    Popped up

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let createForm = (data)=>{
     content.innerHTML += `<img src=${data.Poster} onerror=alert("Failed to load Image")>`
     content.innerHTML += `<h4>Movie Name : ${data.Title}</h4><br>`+`<h4>Release Year : ${data.Year}</h4><br>`+ `<h4>Genre : ${data.Genre}</h4><br>`
     content.innerHTML += `<h4>Plot : ${data.Plot}</h4><br>`+`<h4>Actors : ${data.Actors}</h4><br>`
-    y.appendChild(content)
+    document.body.appendChild(content)
 }
 
 document.addEventListener('submit',(e)=>{
@@ -40,7 +40,6 @@ function createRaindrops() {
     }
   }
   
-  // Запускаємо створення крапель дощу під час завантаження сторінки
 window.addEventListener('load', createRaindrops);
 
 
