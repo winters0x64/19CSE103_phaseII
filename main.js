e26e9a1d080c9c73483e2259fab76dfd42883606
winters0x64 <arun0x168codes@gmail.com>
2023-07-29 13:23:45 +0530

    ok

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ document.addEventListener('submit',(e)=>{
 function createRaindrops() {
     const rainContainer = document.querySelector('.rain');
     const dropCount = 25;
-  
     for (let i = 0; i < dropCount; i++) {
       const drop = document.createElement('div');
       drop.className = 'drop';
